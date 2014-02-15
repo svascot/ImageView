fa8e0c555eef6fb5c934f7630c0ce6200d9ea702
svascot <svascot@gmail.com>
2014-02-15 15:59:51 -0500

    Delete connection.php

--- controller/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$connectionLogin = mysql_connect("localhost","root","qhaWPR76") or die ("connection failed");
-
-$db = mysql_select_db("imageview_login",$connectionLogin) or die ("Incorrect data base");
-
-
-?>
